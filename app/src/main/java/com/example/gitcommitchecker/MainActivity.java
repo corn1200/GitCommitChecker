3cb19322d236ecd6777c1a73e4eb9593cccbd0c3
corn1200 <corn120033@gmail.com>
2022-02-14 21:00:34 +0900

    GitHub API 에 HTTP 요청을 보내고 TextView 에 결과 값을 출력하도록 합니다.

--- app/src/main/java/com/example/gitcommitchecker/MainActivity.java
+++ app/src/main/java/com/example/gitcommitchecker/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.gitcommitchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.text1);
+        String resultText = "[NULL]";
+
+        Log.i("qwer", resultText);
+
+        try {
+            resultText = new Task().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("qwer", resultText);
+
+        textView.setText(resultText);
     }
 }
