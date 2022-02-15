187c585f410d03949e9dd598156519e1ced7c678
corn1200 <corn120033@gmail.com>
2022-02-15 23:53:35 +0900

    버튼 클릭 시 리퀘스트를 보내도록 수정합니다

--- app/src/main/java/com/example/gitcommitchecker/MainActivity.java
+++ app/src/main/java/com/example/gitcommitchecker/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
@@ -16,20 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.text1);
-        String resultText = "[NULL]";
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String resultText = "[NULL]";
 
-        Log.i("qwer", resultText);
+                Log.i("qwer", resultText);
 
-        try {
-            resultText = new Task().execute().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+                try {
+                    resultText = new Task().execute().get();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
 
-        Log.i("qwer", resultText);
+                Log.i("qwer", resultText);
 
-        textView.setText(resultText);
+                textView.setText(resultText);
+            }
+        });
+
     }
 }
