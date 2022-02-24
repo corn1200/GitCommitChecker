ea5814c45df626df6efca1c3ee9f44a28a1b0630
corn1200 <corn120033@gmail.com>
2022-02-24 14:25:01 +0900

    repository DTO 클래스에 주석을 추가합니다

--- app/src/main/java/com/example/gitcommitchecker/Repo.java
+++ app/src/main/java/com/example/gitcommitchecker/Repo.java
@@ -2,7 +2,9 @@ package com.example.gitcommitchecker;
 
 import com.google.gson.annotations.SerializedName;
 
+//    GitHub API 에서 호출할 repository 데이터의 DTO 클래스입니다
 public class Repo {
+//    repository 의 가장 최근 커밋 날짜 정보
     @SerializedName("pushed_at")
     private String pushedAt;
 
