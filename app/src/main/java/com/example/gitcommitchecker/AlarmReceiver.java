b442cccae383d65028c35f575a91b4244bb36d69
corn1200 <corn120033@gmail.com>
2022-02-25 11:17:05 +0900

    알람을 실행하는 브로드캐스트 리시버를 추가합니다

---
+++ app/src/main/java/com/example/gitcommitchecker/AlarmReceiver.java
@@ -0,0 +1,44 @@
+package com.example.gitcommitchecker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.PowerManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private static PowerManager.WakeLock cpuWakeLock;
+    private static WifiManager.WifiLock wifiLock;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        wifiLock = wifiManager.createWifiLock("wifilock");
+        wifiLock.setReferenceCounted(true);
+        wifiLock.acquire();
+
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+
+        cpuWakeLock = powerManager.newWakeLock(
+                PowerManager.PARTIAL_WAKE_LOCK |
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP |
+                        PowerManager.ON_AFTER_RELEASE, "app:alarm");
+
+        cpuWakeLock.acquire();
+
+        Toast.makeText(context, "qwerqwre", Toast.LENGTH_SHORT).show();
+        Log.d("qwer", "Alarm On");
+
+        if (wifiLock != null) {
+            wifiLock.release();
+            wifiLock = null;
+        }
+
+        if (cpuWakeLock != null) {
+            cpuWakeLock.release();
+            cpuWakeLock = null;
+        }
+    }
+}
